df7fc0d09976106207e4b263e94d7ffee0103d6f
rijin113 <rijin113@gmail.com>
2020-07-27 11:46:09 -0400

    Update encrypt_decrypt_app.py
    Version 1.0

--- encrypt_decrypt_app.py
+++ encrypt_decrypt_app.py
@@ -1,4 +1,6 @@
-import random
+#Version 1.0
+import sys
+
 letter = ["a", "b" , "c", "d", "e",
           "f", "g", "h", "i", "j",
           "k", "l", "m", "n", "o",
@@ -7,21 +9,18 @@ letter = ["a", "b" , "c", "d", "e",
 
 character = list(input("Enter a 26 character code of your choice that you want for encryption or decryption: ")) 
 
-#space_key = list(input("Enter a 4 character code for spaces in your sentence:"))
+if len(character) < 26:
+  print("Insufficient characters")
+  sys.exit
+  
+elif len(character) > 26:
+  print("excess characters")
+  sys.exit
+  
 
-#  def check_key():
-#    for n in space_key:
-#      if n == character[1,2,3,4] :
-#        print("Enter a different code")
-#        check_key()
-#      else:
-#        startup()
 key_code = {key:value for key, value in zip(letter, character)}
 
 reverse_dict = {key:value for key, value in zip(character, letter)}
-
-#key_code[" "] = random.choice(space_key)
-#reverse_dict[" "] = random.choice(space_key)
 
 codedlist= []
 
@@ -33,19 +32,17 @@ def startup():
     encodetext()
   elif prompt1.lower() =="d":
     reverse()
+
     
 def encodetext():
   statement = str(input("Enter the statement to be encrypted:").lower())
-  for i in statement:#s
+  for i in statement:
     updated_list = codedlist.append(key_code.get(i))
   encryptedstring=""
   for element in codedlist:
     encryptedstring += element 
   print(encryptedstring)
-  #f = open(r"#enter a path on your computer", "a", encoding="utf-8")
-  #f.write(encryptedstring)
-  #f.write("\n")
-  #f.close
+
 
 def reverse():
   statement2 = str(input("Enter the statement to be decrypted:").lower())
@@ -55,8 +52,5 @@ def reverse():
   for element in reverselist:
     decryptedstring += element 
   print(decryptedstring)
-  #f = open(r"enter you path location", "a", encoding="utf-8")
-  #f.write(decryptedstring)
-  #f.close
 
 startup()
